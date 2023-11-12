b00f3d7b333b7be4b0d655af6aa1873ff826072e
rickyly1 <lyr5@uci.edu>
2023-11-12 14:08:52 -0800

    project section content and responsive formatting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,11 +5,13 @@ import "./Projects.css";
 // project screenshots
 import GitHubLogo from './assets/github.png';
 import YourEdvisor from './assets/youredvisor.PNG';
+import Blog from './assets/blog.PNG';
+import ToDo from './assets/todolist.PNG';
 
 function Projects() {
     return (
         <div className="project-container">
-            <h1>Projects</h1>
+            <h1>Some Recent Projects</h1>
 
             <div className="card-container">
                 <div className="card">
@@ -20,32 +22,39 @@ function Projects() {
                     </p>
 
                     <div className="card-subtext">
-                        <a href="https://github.com/rickyly1" className="logo-link" target="_blank" rel="noreferrer"><img src={GitHubLogo} alt="GitHub Logo"/>Codebase</a>
+                        <a href="https://github.com/rickyly1/youredvisor" className="logo-link" target="_blank" rel="noreferrer"><img src={GitHubLogo} alt="GitHub Logo"/>Codebase</a>
                         <h4>HTML/CSS/JS, Bootstrap</h4>
                     </div>
                 </div>
 
-                {/** 
                 <div className="card">
                     <h2>Personal Blog</h2>
-                    <img src={YourEdvisor} alt='A college matriculation prototype' className="preview"/>
+                    <img src={Blog} alt='A personal blog' className="preview"/>
                     <p>
-                        A college matriculation service designed and prototyped as part of my senior capstone project at UC Irvine.
+                        A personal blog built with MongoDB that also utilizes authentification and verification. Posts can be written, edited, and deleted.
                     </p>
-                    <a href="https://github.com/rickyly1" className="logo-link" target="_blank" rel="noreferrer"><img src={GitHubLogo} alt="GitHub Logo"/>Codebase</a>
-                    <h4>HTML/CSS/JS, Bootstrap</h4>
+
+                    <div className="card-subtext">
+                        <a href="https://github.com/rickyly1/personal-blog" className="logo-link" target="_blank" rel="noreferrer"><img src={GitHubLogo} alt="GitHub Logo"/>Codebase</a>
+                        <h4>Node.Js, MongoDB, Express</h4>
+                    </div>
                 </div>
 
                 <div className="card">
-                    <h2>YourEdvisor</h2>
-                    <img src={YourEdvisor} alt='A college matriculation prototype' className="preview"/>
+                    <h2>To-Do List</h2>
+                    <img src={ToDo} alt='A to-do list' className="preview"/>
                     <p>
-                        A college matriculation service designed and prototyped as part of my senior capstone project at UC Irvine.
+                        A to-do list utilizing local storage with features including categorization and item editing and deletion.
                     </p>
-                    <a href="https://github.com/rickyly1" className="logo-link" target="_blank" rel="noreferrer"><img src={GitHubLogo} alt="GitHub Logo"/>Codebase</a>
-                    <h4>HTML/CSS/JS, Bootstrap</h4>
-                </div>  */}
+
+                    <div className="card-subtext">
+                        <a href="https://github.com/rickyly1/ToDo-List" className="logo-link" target="_blank" rel="noreferrer"><img src={GitHubLogo} alt="GitHub Logo"/>Codebase</a>
+                        <h4>HTML/CSS/JS</h4>
+                    </div>
+                </div>
             </div>
+
+            <p id='project-call'>Check out some of my other works at <a href="https://github.com/rickyly1" target='_blank' rel='noreferrer'>my Github</a>!</p>
         </div>
     );
 }
